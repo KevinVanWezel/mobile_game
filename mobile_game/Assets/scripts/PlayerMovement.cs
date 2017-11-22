c04a145b837df3bdc07b2a90566decf353ff0645
KevinVanWezel <21973@ma-web.nl>
2017-11-22 11:07:27 +0100

    i hope thsi works

---
+++ mobile_game/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody _rigidbody;
+    private Vector3 _inputDirection;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+
+    }
+   
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(Vector3.up * 50 * Time.deltaTime, Space.World);
+        }
+       
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        _inputDirection = new Vector3(x, 0f, z);
+
+
+    }
+    void FixedUpdate()
+    {
+       
+        _rigidbody.AddForce(_inputDirection * speed);
+    }
+   
+
+   
+}
---
+++ mobile_game/Assets/scripts/objectsMoving.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectsMoving : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public Vector3 userDirection = Vector3.right;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        transform.position += userDirection * 4 * Time.deltaTime;
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        {
+            if (other.collider.CompareTag("walls"))
+            {
+                if (userDirection == Vector3.left)
+                {
+                    userDirection = Vector3.right;
+                }
+                else
+                {
+                    userDirection = Vector3.left;
+                }
+            }
+
+
+
+        }
+    }
+}
