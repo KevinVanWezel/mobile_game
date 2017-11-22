9aef51b64e8c922868635a50972a4b6d62fbde05
neocccro <cavandiepen@gmail.com>
2017-11-22 12:15:54 +0100

    gyro

---
+++ mobile_game/Assets/scripts/Mobile_Rolling_Ball.cs
@@ -0,0 +1,38 @@
+﻿// iOS gyroscope example
+//
+// Create a cube with camera vector names on the faces.
+// Allow the iPhone to show named faces as it is oriented.
+
+using UnityEngine;
+
+public class Mobile_Rolling_Ball : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // make a quad for one side of the cube
+    protected void Update()
+    {
+        Input.gyro.enabled = true;
+        //GyroModifyBall();
+    }
+    
+
+    /********************************************/
+
+    // The Gyroscope is right-handed.  Unity is left handed.
+    // Make the necessary change to the camera.
+    void GyroModifyBall()
+    {
+        rb.AddForce(GyroToForce(Input.gyro.attitude));
+    }
+
+    private Vector3 GyroToForce(Quaternion q)
+    {
+        return new Vector3(q.x, q.y, -q.z);
+    }
+}
