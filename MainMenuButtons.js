b8dd538390506aa12bd58c7a9effe89817d86e9a
joshuapole99 <joshuapole@live.nl>
2017-11-22 12:37:33 +0100

    menu scene and scripts

---
+++ MainMenuButtons.js
@@ -0,0 +1,20 @@
+﻿function PlayGame() {
+    Application.LoadLevel(1);
+}
+
+function QuitGame() {
+    Application.Quit();
+}
+
+function MainGame() {
+    Application.LoadLevel(0);
+}
+
+function WinGame() {
+    Application.LoadLevel(2);
+}
+
+function LoseGame() {
+    Application.LoadLevel(3);
+}
+
